82ffa7b57c62aa0d0f471764c0377481ad252dcb
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-11-06 10:58:42 +0000

    Include random element to genUuid

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -50,7 +50,7 @@ module.exports.bootModule = Ground.bootModule;
 
 // These aren't so much "Universal" as they are "VM-wide-unique".
 let uuidIndex = 0;
-let uuidPrefix = '__@syndicate__' + RandomID.randomId(8);
+let uuidInstance = RandomID.randomId(8);
 module.exports.genUuid = function (prefix) {
-  return (prefix || uuidPrefix) + '_' + uuidIndex++;
+  return (prefix || '__@syndicate') + '_' + uuidInstance + '_' + uuidIndex++;
 };
