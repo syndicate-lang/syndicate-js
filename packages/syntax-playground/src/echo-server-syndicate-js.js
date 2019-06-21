4cd8b58d1aedbf10dc4d84b7a15aefb0e6b1e72c
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2019-06-21 17:23:08 +0100

    Terminate server once last connection goes

--- packages/syntax-playground/src/echo-server-syndicate-js.js
+++ packages/syntax-playground/src/echo-server-syndicate-js.js
@@ -1,10 +1,14 @@
 const S = activate require("@syndicate-lang/driver-streams-node");
 
+assertion type ConnectionActive();
+
 spawn named 'echoserver' {
   on asserted S.Stream($id, S.Incoming(S.TcpListener(5999))) {
     spawn {
+      assert ConnectionActive();
       stop on retracted S.Stream(id, S.Duplex());
       on message S.Stream(id, S.Data($bs)) send S.Stream(id, S.Push(bs, false));
     }
   }
+  stop on retracted ConnectionActive();
 }
